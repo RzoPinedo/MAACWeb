d2cc146d92ced1f947f2b030baeb3f84c4d58eb7
RzoPinedo <rzopinedo@gmail.com>
2022-09-25 23:20:41 -0500

    inicio de aplicacion web

---
+++ src/main/java/pe/idat/g7/maacweb/MaacWebApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.g7.maacweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaacWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaacWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/g7/maacweb/MaacWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.g7.maacweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaacWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
